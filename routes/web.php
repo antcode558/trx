abbee09bff4d52c9f0c65e356843774d3d7336a3
Administrator <ewew@qq.com>
2024-07-04 12:39:03 +0800

    提交测试

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/notify', function (Request $request) {
+    echo 'notifyGet';
+
+    echo $request;
+
+    return $request;
+});
+
+Route::post('/notifyPost', function (Request $request) {
+
+    echo 'notifyPost';
+    echo $request;
+
+    return $request;
 });
