a3b5e3743cc62c5015fd2e9b7243f2e071e935ff
Administrator <ewew@qq.com>
2024-07-05 14:17:57 +0800

    添加vendor

---
+++ app/Http/Kernel.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http;
+
+use App\Http\Middleware\Login;
+use App\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * These middleware are run during every request to your application.
+     *
+     * @var array
+     */
+    protected $middleware = [
+//        \App\Http\Middleware\CheckForMaintenanceMode::class,
+//        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+//        \App\Http\Middleware\TrimStrings::class,
+//        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\TrustProxies::class,
+    ];
+
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [
+        'web' => [
+
+
+//            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+//            \Illuminate\Session\Middleware\StartSession::class,
+//            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+//            \App\Http\Middleware\VerifyCsrfToken::class,
+//            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'api' => [
+            'throttle:60,1',
+            'bindings',
+        ],
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * These middleware may be assigned to groups or used individually.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+
+    ];
+
+    /**
+     * The priority-sorted list of middleware.
+     *
+     * This forces non-global middleware to always be in the given order.
+     *
+     * @var array
+     */
+    protected $middlewarePriority = [
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,78 @@ Route::get('/notify', function (Request $request) {
 
     echo $request;
 
+    $url = 'https://api.telegram.org/bot7043828057:AAFI5aLJvtCgo-aIZO8Q-6PDiSctCxZ79yU/sendMessage?chat_id=6635332924&text=1234';
+
+    $res = Get_Pay($url);
+
+
+//    $url2 = 'https://api.telegram.org/bot7043828057:AAFI5aLJvtCgo-aIZO8Q-6PDiSctCxZ79yU/sendMessage?chat_id=6635332924&text=' . $res;
+
+//    $postUrl = 'http://127.0.0.1:8000/notifyPost';
+//    $re = post_url($postUrl);
+//    echo $re;
+
+//    echo $res;
+
     return $request;
 });
+
 
 Route::post('/notifyPost', function (Request $request) {
 
-    echo 'notifyPost';
-    echo $request;
+//    $url1 = 'https://api.telegram.org/bot7043828057:AAFI5aLJvtCgo-aIZO8Q-6PDiSctCxZ79yU/sendMessage?chat_id=6635332924&text=post';
+//
+//    Get_Pay($url1);
+
 
     return $request;
 });
+
+/**
+ * API请求
+ * @param $url  链接
+ * @param $data  参数
+ */
+function Get_Pay($url, $data = null, array $heders = [], $time = 0)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_TIMEOUT, $time);          //单位 秒，也可以使用
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //这个是重点,规避ssl的证书检查。
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); // 跳过host验证
+    if (!empty($data)) {
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    }
+    if (!empty($heders)) {
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $heders);
+    }
+    $content = curl_exec($ch);
+    curl_close($ch);
+    return $content;
+}
+
+/**
+ * post请求
+ * @param $url  string api链接
+ * @param $url  array 参数
+ * @param $time_out int 超时时间
+ */
+function post_url(string $url, array $data = [], $time_out = 5)
+{
+    $data = json_encode($data);
+    $headerArray = array("Content-type: application/json;charset='utf-8'");
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $time_out);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headerArray);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($curl);
+    curl_close($curl);
+    return json_decode($output, true);
+}
